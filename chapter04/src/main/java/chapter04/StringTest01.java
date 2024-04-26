f48f93c4ef933eca31ae791a245f07213d86e4dd
kimjiheej <glp0509@naver.com>
2024-04-26 15:07:39 +0900

    StringTest02

---
+++ chapter04/src/main/java/chapter04/StringTest01.java
@@ -0,0 +1,35 @@
+package chapter04;
+
+public class StringTest01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// c:\temp 라고 출력하고 싶다 
+		
+		// c:\temp 
+		System.out.println("c:\\temp"); // 역슬래쉬 
+		
+	   // "Hello" 
+		
+		System.out.println("\"hello\"");
+		
+		// \t: tab 
+		
+		
+		// hello\n\nworld 
+		
+		// \r : carriage return 
+		// \n : newline 
+	    // \b: beep 
+		
+		System.out.println("hello\tworld\n");
+		System.out.println("hello\tworld");
+		
+		char c = '\'';
+		System.out.println(c);
+		
+
+	}
+
+}
