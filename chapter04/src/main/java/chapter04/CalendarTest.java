846be902801e20d8600c76babb3ecca29a807dda
kimjiheej <glp0509@naver.com>
2024-04-29 14:30:00 +0900

    Calendar #finish

--- chapter04/src/main/java/chapter04/CalendarTest.java
+++ chapter04/src/main/java/chapter04/CalendarTest.java
@@ -14,15 +14,45 @@ public class CalendarTest {
 		
 	   // i 18 n Locale 을 적어놓으면 많이 잡히게 된다 
 		
-	   Calendar.getInstance();
+	   Calendar cal = Calendar.getInstance();
 	   
 //	   Locale aLocale = Locale.getDefault(Locale.Category.FORMAT);
 //	   TimeZone tz = TimeZone.getDefault();
 	 //  System.out.println(aLocale+":" + tz);
 	   
-	   System.out.println();
+	   System.out.println(cal);
 	   
+	   printDate(cal);
 	   
+	   cal.set(Calendar.YEAR, 2024);
+	   cal.set(Calendar.MONDAY, 11); // 12월(Month -1) 
+	   cal.set(Calendar.DATE, 25);
+	   printDate(cal);
+	   
+	   cal.set(2001, 03, 04);
+	   cal.add(Calendar.DATE, 10000);
+	}
+
+	private static void printDate(Calendar cal) {
+		// TODO Auto-generated method stub
+		
+		final String[] DAYS = {"일","월","화","수","목","금","토"};
+		
+		int year = cal.get(Calendar.YEAR);
+		
+	    int month = cal.get(Calendar.MONDAY);
+	    
+	    int date = cal.get(Calendar.DATE);
+	    
+	    int day = cal.get(Calendar.DAY_OF_WEEK);  // 1(일) ~ 7 (
+	    
+	    int hour = cal.get(Calendar.HOUR);
+	    
+	    int minute = cal.get(Calendar.MINUTE);
+	    
+	    int second = cal.get(Calendar.SECOND);
+	    
+	    System.out.println(year + " 년 " + (month+1) + "월 " + date + "일 " + DAYS[day-1] + "요일 "  + hour + "시 "+ minute + "분 "+ second + "초");
 	}
 
 }
