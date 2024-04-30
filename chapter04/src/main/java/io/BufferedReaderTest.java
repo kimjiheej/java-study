54a55228b14b11ef69d661aab907c0f33f3c240a
kimjiheej <glp0509@naver.com>
2024-04-30 14:35:10 +0900

    Stream

--- chapter04/src/main/java/io/BufferedReaderTest.java
+++ chapter04/src/main/java/io/BufferedReaderTest.java
@@ -8,37 +8,33 @@ import java.io.IOException;
 public class BufferedReaderTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-     
 		BufferedReader br = null;
 		
-		// 기반 스트림 
-	   try {
-		FileReader fr = new FileReader("./src/main/java/io/BufferedRederTest.java");
-		// 보조 스트림 
-		
-	   br = new BufferedReader(fr);
-	   
-	   String line = null;
-	   
-	   while((line = br.readLine()) != null) {
-		   System.out.println(line);
-	   }
-		
-	} catch (FileNotFoundException e) {
-		System.out.println("file not found: "+ e);
-	} catch (IOException e) {
-		System.out.println("erorr:" + e);
-	}
-	   finally {
 		try {
-			if( br != null) {
-				br.close();
+			// 기반 스트림
+			FileReader fr = new FileReader("./src/main/java/io/BufferedReaderTest.java");
+			
+			// 보조 스트림
+			br = new BufferedReader(fr);
+			
+			String line = null;
+			
+			while((line = br.readLine()) != null) {
+				System.out.println(line);
 			}
+		} catch (FileNotFoundException e) {
+			System.out.println("file not found:" + e);
 		} catch (IOException e) {
-			e.printStackTrace();
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if(br != null) {
+					br.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
-	}
 	}
 
 }
