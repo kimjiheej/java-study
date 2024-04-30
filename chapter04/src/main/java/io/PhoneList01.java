e9e03303fe37c8688c9e30a6cbaf013066823f4c
kimjiheej <glp0509@naver.com>
2024-04-30 13:51:56 +0900

    stream

---
+++ chapter04/src/main/java/io/PhoneList01.java
@@ -0,0 +1,25 @@
+package io;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class PhoneList01 {
+
+	public static void main(String[] args) {
+		try {
+			File file = new File("./phone.txt");
+			if(!file.exists()) {
+				System.out.println("file not found");
+				return;
+			}
+			
+			FileInputStream fis = new FileInputStream(file);
+			fis.read();
+		}  catch (IOException e) {
+			
+		} 
+	}
+
+}
