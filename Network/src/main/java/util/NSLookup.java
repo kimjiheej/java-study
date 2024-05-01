c6788c5feaacf2aeb3a78170daefd6c5ba567507
kimjiheej <glp0509@naver.com>
2024-05-01 20:22:14 +0900

    NSLookup_assignment

--- Network/src/main/java/util/NSLookup.java
+++ Network/src/main/java/util/NSLookup.java
@@ -4,6 +4,7 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
+
 public class NSLookup {
 
     public static void main(String[] args) {
@@ -15,16 +16,18 @@ public class NSLookup {
             System.out.print("> ");
             s = sc.nextLine();
             
-            try {
-                InetAddress[] inetAddresses = InetAddress.getAllByName(s);
-                for(InetAddress a : inetAddresses) {
-                    System.out.println(a);
+            if (!s.equals("exit")) {
+                try {
+                    InetAddress[] inetAddresses = InetAddress.getAllByName(s);
+                    for(InetAddress a : inetAddresses) {
+                        System.out.println(a);
+                    }
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
                 }
-            } catch (UnknownHostException e) {
-                e.printStackTrace();
             }
         } while (!s.equals("exit"));
         
-        sc.close(); // Scanner를 사용한 후에는 close() 메소드를 호출하여 리소스를 해제하는 것이 좋습니다.
+        sc.close(); 
     }
 }
